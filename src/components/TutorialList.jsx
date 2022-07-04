014c41b3b4602abee0e52a2476148c49f4553097
Sadullah TANRIKULU <sadullahtanrikulu@gmail.com>
2022-07-04 23:06:31 +0300

    in class edit process study

--- src/components/TutorialList.jsx
+++ src/components/TutorialList.jsx
@@ -1,21 +1,11 @@
 import { FaEdit } from 'react-icons/fa';
 import { AiFillDelete } from 'react-icons/ai';
 import EditTutorial from './EditTutorial';
+import { useState } from 'react';
 
-const TutorialList = ({tutorials, deleteTutorial, editTutorial}) => {
-  //? Test data
-  // let tutorials = [
-  //   {
-  //     id: 1,
-  //     title: 'ReactJS',
-  //     description: 'React is a JS-library for UI Design',
-  //   },
-  //   {
-  //     id: 2,
-  //     title: 'HTML',
-  //     description: 'HTML is a markup language',
-  //   },
-  // ];
+const TutorialList = ({ tutorials, deleteTutorial, editTutorial }) => {
+  const [editItem, setEditItem] = useState('');
+
   return (
     <div className="container mt-4">
       <table className="table table-striped">
@@ -37,19 +27,27 @@ const TutorialList = ({tutorials, deleteTutorial, editTutorial}) => {
                 <th>{id}</th>
                 <td>{title}</td>
                 <td>{description}</td>
-                <td className="text-center">
-                  <FaEdit 
-                  data-bs-toggle="modal"
-                  data-bs-target="#edit-modal"
-                  size={20} className="me-3 text-warning cursor-pointer" onClick={() => editTutorial(id, 'TailwindCSS', 'is the Best')} />
-                  <AiFillDelete size={22} className="text-danger cursor-pointer" onClick={() => deleteTutorial(id)} />
+                <td className="text-center text-nowrap">
+                  <FaEdit
+                    data-bs-toggle="modal"
+                    data-bs-target="#edit-modal"
+                    size={20}
+                    className="me-2 text-warning cursor-pointer"
+                    onClick={() => setEditItem(item)}
+                  />
+                  <AiFillDelete
+                    size={22}
+                    className="text-danger cursor-pointer"
+                    onClick={() => deleteTutorial(id)}
+                  />
                 </td>
               </tr>
             );
           })}
         </tbody>
       </table>
-      <EditTutorial />
+
+      <EditTutorial editTutorial={editTutorial} editItem={editItem} />
     </div>
   );
 };
